6537e0733e6cd38734d770ab2b8200d49c2932ea
Romain Jacotin <romain.jacotin@orange.fr>
2015-06-08 12:42:41 +0200

    Final Elliptic Curve Diffie-Hellman P-256 KeyExchange

--- crypto/p256.go
+++ crypto/p256.go
@@ -1,21 +1,38 @@
 package crypto
 
+import "crypto/elliptic"
+import "crypto/rand"
+import "math/big"
+
 type p256 struct {
-	publicKey  []byte
+	curve      elliptic.Curve
+	publicX    *big.Int
+	publicY    *big.Int
 	privateKey []byte
 }
 
 // NewECDH_P256 returns an Elliptic Curve Diffie-Hellman P-256 KeyExchange algorithm.
 func NewECDH_P256() (error, KeyExchange) {
-	return nil, &p256{}
+	curve := elliptic.P256()
+	priv, x, y, err := elliptic.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		return err, nil
+	}
+	return nil, &p256{
+		curve:      curve,
+		publicX:    x,
+		publicY:    y,
+		privateKey: priv}
 }
 
 // GetPublicKey generates local private/public keys pair and returns the local public key that should be sent to the remote host.
 func (this *p256) GetPublicKey() []byte {
-	return nil
+	return elliptic.Marshal(this.curve, this.publicX, this.publicX)
 }
 
-// ComputeSharedKey computes and returns the shared key based on the local private key and the remote public key described in input.
-func (this *p256) ComputeSharedKey([]byte) []byte {
-	return nil
+// ComputeSharedKey computes and returns the shared key based on the local private key and the remote public key.
+func (this *p256) ComputeSharedKey(remotePublicKey []byte) []byte {
+	remotePublicX, remotePublicY := elliptic.Unmarshal(this.curve, remotePublicKey)
+	x, _ := this.curve.ScalarMult(remotePublicX, remotePublicY, this.privateKey)
+	return x.Bytes()
 }
