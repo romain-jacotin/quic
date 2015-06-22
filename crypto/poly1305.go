36d5578e7915941f42b7f77ddc401f5fe1f832ed
Romain Jacotin <romain.jacotin@orange.fr>
2015-06-22 17:59:45 +0200

    Adding Poly1305 Message Authentication Code for AEAD construction with respect to RFC7539, and with successful go test (test vector from RFC7539)

--- crypto/poly1305.go
+++ crypto/poly1305.go
@@ -3,6 +3,13 @@ package crypto
 import "errors"
 import "encoding/binary"
 
+const (
+	aeadEnd = iota
+	aeadAAD
+	aeadCipherText
+	aeadLengths
+)
+
 type Poly1305 struct {
 	r0, r1, r2                       uint64 // r_key coded in a uint130 (44-bit + 44-bit + 42-bit)
 	s_key_begin, s_key_end           uint64 // s_key coded in two uint64
@@ -61,7 +68,7 @@ func NewPoly1305(key []byte) (*Poly1305, error) {
 	return p, nil
 }
 
-func (this *Poly1305) ComputeHash(data []byte) (high_mac, low_mac uint64) {
+func (this *Poly1305) ComputeMAC(data []byte) (high_mac, low_mac uint64) {
 	var r0, r1, r2, h0, h1, h2, c, c0, c1, c2 uint64
 	var i, j, l uint
 	var d, d0, d1, d2 [4]uint64
@@ -113,12 +120,513 @@ func (this *Poly1305) ComputeHash(data []byte) (high_mac, low_mac uint64) {
 		// if chunk'size == 16 bytes then add a 17th byte = 0x01
 		// if chunk'size  < 16 bytes then add a last byte = 0x01, and bytes up to 17th are equals to 0
 		if j < 6 {
-			c0 |= 1 << (j * 8)
+			c0 |= 1 << (j << 3)
 		} else if j < 11 {
-			c1 |= 1 << (4 + (j-6)*8)
+			c1 |= 1 << (4 + ((j - 6) << 3))
 		} else { // j >= 11
-			c2 |= 1 << ((j - 11) * 8)
+			c2 |= 1 << ((j - 11) << 3)
 		}
+
+		// for each chunk 'c', update 'h' like this:      h = ((h + c) * r) % ((2^130)-5)
+
+		// Calculate h = h + c
+		h0 += c0
+		h1 += c1
+		h2 += c2
+
+		// Calculate h = h * r
+
+		// d0 = h0*r0 + h1*(r2*(5<<2)) + h2*(r1*(5<<2))
+		//   step 1: d0 = h0*r0
+		//   step 2: d  = h1*s2 (with s2 = r2*(5<<2))
+		//   step 3: d0 += d
+		//   step 4: d  = h2*s1 (with s1 = r1*(5<<2))
+		//   step 5: d0 += d
+
+		// step 1: d0 = h0*r0
+		a0 = h0 & 0xffffffff
+		a1 = h0 >> 32
+		b0 = r0 & 0xffffffff
+		b1 = r0 >> 32
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d0[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d0[1] = dst1 & 0xffffffff
+		d0[2] = dst2 & 0xffffffff
+		d0[3] = dst3 & 0xffffffff
+
+		// step 2: d = h1*s2
+		a0 = h1 & 0xffffffff
+		a1 = h1 >> 32
+		b0 = this.s2_low
+		b1 = this.s2_high
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d[1] = dst1 & 0xffffffff
+		d[2] = dst2 & 0xffffffff
+		d[3] = dst3 & 0xffffffff
+
+		// step 3: d0 += d
+		dst0 = d0[0] + d[0]
+		dst1 = d0[1] + d[1]
+		dst2 = d0[2] + d[2]
+		dst3 = d0[3] + d[3]
+		dst1 += dst0 >> 32
+		d0[0] = dst0 & 0xffffffff
+		dst2 = dst2 + (dst1 >> 32)
+		d0[1] = dst1 & 0xffffffff
+		dst3 += (dst2 >> 32)
+		d0[2] = dst2 & 0xffffffff
+		d0[3] = dst3 & 0xffffffff
+
+		// step 4: d = h2*s1
+		a0 = h2 & 0xffffffff
+		a1 = h2 >> 32
+		b0 = this.s1_low
+		b1 = this.s1_high
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d[1] = dst1 & 0xffffffff
+		d[2] = dst2 & 0xffffffff
+		d[3] = dst3 & 0xffffffff
+
+		// step 5: d0 += d
+		dst0 := d0[0] + d[0]
+		dst1 := d0[1] + d[1]
+		dst2 := d0[2] + d[2]
+		dst3 := d0[3] + d[3]
+		dst1 += dst0 >> 32
+		d0[0] = dst0 & 0xffffffff
+		dst2 = dst2 + (dst1 >> 32)
+		d0[1] = dst1 & 0xffffffff
+		dst3 += (dst2 >> 32)
+		d0[2] = dst2 & 0xffffffff
+		d0[3] = dst3 & 0xffffffff
+
+		// d1 = h0*r1 + h1*r0 + h2*(r2*(5<<2))
+		//   step 1: d1 = h0*r1
+		//   step 2: d  = h1*r0
+		//   step 3: d1 += d
+		//   step 4: d  = h2*s2 (with s2 = r2*(5<<2))
+		//   step 5: d1 += d
+
+		// step 1: d1 = h0*r1
+		a0 = h0 & 0xffffffff
+		a1 = h0 >> 32
+		b0 = r1 & 0xffffffff
+		b1 = r1 >> 32
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d1[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d1[1] = dst1 & 0xffffffff
+		d1[2] = dst2 & 0xffffffff
+		d1[3] = dst3 & 0xffffffff
+
+		// step 2: d = h1*r0
+		a0 = h1 & 0xffffffff
+		a1 = h1 >> 32
+		b0 = r0 & 0xffffffff
+		b1 = r0 >> 32
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d[1] = dst1 & 0xffffffff
+		d[2] = dst2 & 0xffffffff
+		d[3] = dst3 & 0xffffffff
+
+		// step 3: d1 += d
+		dst0 = d1[0] + d[0]
+		dst1 = d1[1] + d[1]
+		dst2 = d1[2] + d[2]
+		dst3 = d1[3] + d[3]
+		dst1 += dst0 >> 32
+		d1[0] = dst0 & 0xffffffff
+		dst2 = dst2 + (dst1 >> 32)
+		d1[1] = dst1 & 0xffffffff
+		dst3 += (dst2 >> 32)
+		d1[2] = dst2 & 0xffffffff
+		d1[3] = dst3 & 0xffffffff
+
+		// step 4: d = h2*s2
+		a0 = h2 & 0xffffffff
+		a1 = h2 >> 32
+		b0 = this.s2_low
+		b1 = this.s2_high
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d[1] = dst1 & 0xffffffff
+		d[2] = dst2 & 0xffffffff
+		d[3] = dst3 & 0xffffffff
+
+		// step 5: d1 += d
+		dst0 = d1[0] + d[0]
+		dst1 = d1[1] + d[1]
+		dst2 = d1[2] + d[2]
+		dst3 = d1[3] + d[3]
+		dst1 += dst0 >> 32
+		d1[0] = dst0 & 0xffffffff
+		dst2 = dst2 + (dst1 >> 32)
+		d1[1] = dst1 & 0xffffffff
+		dst3 += (dst2 >> 32)
+		d1[2] = dst2 & 0xffffffff
+		d1[3] = dst3 & 0xffffffff
+
+		// d2 = h0*r2 + h1*r1 + h2*r0
+		//   step 1: d2 = h0*r2
+		//   step 2: d  = h1*r1
+		//   step 3: d2 += d
+		//   step 4: d  = h2*r0
+		//   step 5: d2 += d
+
+		// step 1: d2 = h0*r2
+		a0 = h0 & 0xffffffff
+		a1 = h0 >> 32
+		b0 = r2 & 0xffffffff
+		b1 = r2 >> 32
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d2[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d2[1] = dst1 & 0xffffffff
+		d2[2] = dst2 & 0xffffffff
+		d2[3] = dst3 & 0xffffffff
+
+		// step 2: d = h1*r1
+		a0 = h1 & 0xffffffff
+		a1 = h1 >> 32
+		b0 = r1 & 0xffffffff
+		b1 = r1 >> 32
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d[1] = dst1 & 0xffffffff
+		d[2] = dst2 & 0xffffffff
+		d[3] = dst3 & 0xffffffff
+
+		// step 3: d2 += d
+		dst0 = d2[0] + d[0]
+		dst1 = d2[1] + d[1]
+		dst2 = d2[2] + d[2]
+		dst3 = d2[3] + d[3]
+		dst1 += dst0 >> 32
+		d2[0] = dst0 & 0xffffffff
+		dst2 = dst2 + (dst1 >> 32)
+		d2[1] = dst1 & 0xffffffff
+		dst3 += (dst2 >> 32)
+		d2[2] = dst2 & 0xffffffff
+		d2[3] = dst3 & 0xffffffff
+
+		// step 4: d = h2*r0
+		a0 = h2 & 0xffffffff
+		a1 = h2 >> 32
+		b0 = r0 & 0xffffffff
+		b1 = r0 >> 32
+		dst0 = a0 * b0
+		dst1 = dst0 >> 32
+		d[0] = dst0 & 0xffffffff
+		dst1 += a1 * b0
+		dst2 = dst1 >> 32
+		dst1 &= 0xffffffff
+		dst1 += a0 * b1
+		dst2 += dst1 >> 32
+		dst3 = dst2 >> 32
+		dst2 &= 0xffffffff
+		dst2 += a1 * b1
+		dst3 += dst2 >> 32
+		d[1] = dst1 & 0xffffffff
+		d[2] = dst2 & 0xffffffff
+		d[3] = dst3 & 0xffffffff
+
+		// step 5: d2 += d
+		dst0 = d2[0] + d[0]
+		dst1 = d2[1] + d[1]
+		dst2 = d2[2] + d[2]
+		dst3 = d2[3] + d[3]
+		dst1 += dst0 >> 32
+		d2[0] = dst0 & 0xffffffff
+		dst2 = dst2 + (dst1 >> 32)
+		d2[1] = dst1 & 0xffffffff
+		dst3 += (dst2 >> 32)
+		d2[2] = dst2 & 0xffffffff
+		d2[3] = dst3 & 0xffffffff
+
+		// partial h %= ((2^130)-5)
+		// In fact we don't calculate the complete modulo value, but the lowest value that is < 2^130
+
+		// Convert d (= uint128 simulated with 4 uint64 that contains 32+32+32+32 bits)
+		// into h (= uint130 simulated with 3 uint64 that contains 42+44+44) and propagate the carry ( = c )
+
+		// h0 = LSB 44 bits of d0
+		c = (d0[1] >> 12) + (d0[2] << 20) + (d0[3] << 52)
+		h0 = (d0[0] + (d0[1] << 32)) & 0xfffffffffff
+
+		// h1 = LSB 44 bits of d1
+
+		// addlo_reg(&d1, c)
+		dst0 = d1[0] + (c & 0xffffffff)
+		dst1 = d1[1] + (c >> 32)
+		dst1 += dst0 >> 32
+		d1[0] = dst0 & 0xffffffff
+		dst2 = d1[2] + (dst1 >> 32)
+		d1[1] = dst1 & 0xffffffff
+		dst3 = d1[3] + (dst2 >> 32)
+		d1[2] = dst2 & 0xffffffff
+		d1[3] = dst3 & 0xffffffff
+
+		c = (d1[1] >> 12) + (d1[2] << 20) + (d1[3] << 52)
+		h1 = (d1[0] + (d1[1] << 32)) & 0xfffffffffff
+
+		// h1 = LSB 42 bits of d2
+
+		// addlo_reg(&d2, c)
+		dst0 = d2[0] + (c & 0xffffffff)
+		dst1 = d2[1] + (c >> 32)
+		dst1 += dst0 >> 32
+		d2[0] = dst0 & 0xffffffff
+		dst2 = d2[2] + (dst1 >> 32)
+		d2[1] = dst1 & 0xffffffff
+		dst3 = d2[3] + (dst2 >> 32)
+		d2[2] = dst2 & 0xffffffff
+		d2[3] = dst3 & 0xffffffff
+
+		c = (d2[1] >> 10) + (d2[2] << 22) + (d2[3] << 54)
+		h2 = (d2[0] + (d2[1] << 32)) & 0x3ffffffffff
+
+		// Use the carry (= c) to calculate the partial modulo (2^130 - 5)
+		// partial modulo = multiply the 130 bits value by the carry (the carry is the upper bit at the left of the 130 bits)
+		h0 += c * 5
+		c = (h0 >> 44)
+		h0 = h0 & 0xfffffffffff
+		h1 += c
+		// Note: the carry is not fully propagated into h here, the full carry will be made after the last chunk (=after the 'chunk' loop)
+	}
+
+	// Fully carry h
+	c = (h1 >> 44)
+	h1 &= 0xfffffffffff
+	h2 += c
+	c = (h2 >> 42)
+	h2 &= 0x3ffffffffff
+	h0 += c * 5
+	c = (h0 >> 44)
+	h0 &= 0xfffffffffff
+	h1 += c
+	c = (h1 >> 44)
+	h1 &= 0xfffffffffff
+	h2 += c
+	c = (h2 >> 42)
+	h2 &= 0x3ffffffffff
+	h0 += c * 5
+	c = (h0 >> 44)
+	h0 &= 0xfffffffffff
+	h1 += c
+
+	// Now it is the final step to compute h % ((2^130)-5)
+	// We compare 'h' and 'p' :
+	//   if h < p then h is the final modulus value
+	//   if h >= p then the final value is h - p
+	// Compute h - p = h - (2^130 - 5) = h + 5 - 2^130 = h + 5 - (1 << 130)
+	c0 = h0 + 5
+	c = c0 >> 44
+	c0 &= 0xfffffffffff
+	c1 = h1 + c
+	c = c1 >> 44
+	c1 &= 0xfffffffffff
+	c2 = h2 + c - (1 << 42)
+
+	// select h if h < p, or (h - p) if h >= p
+	c = (c2 >> 63) - 1
+	c0 &= c
+	c1 &= c
+	c2 &= c
+	c = ^c
+	h0 = (h0 & c) | c0
+	h1 = (h1 & c) | c1
+	h2 = (h2 & c) | c2
+
+	// Read 's' as Little Endian uint128 (c0 = low 64 bits, c1 = high 64 bits)
+	c0 = this.s_key_begin
+	c1 = this.s_key_end
+
+	// h = h + s (in uint130)
+	h0 += ((c0) & 0xfffffffffff)
+	c = h0 >> 44
+	h0 &= 0xfffffffffff
+
+	h1 += (((c0 >> 44) | (c1 << 20)) & 0xfffffffffff) + c
+	c = h1 >> 44
+	h1 &= 0xfffffffffff
+
+	h2 += ((c1 >> 24) & 0x3ffffffffff) + c
+	h2 &= 0x3ffffffffff
+
+	// Transform h in uint128: h = h % (2^128)
+	h0 = h0 + (h1 << 44)
+	h1 = ((h1 >> 20) + (h2 << 24))
+
+	return h1, h0
+}
+
+func (this *Poly1305) ComputeAeadMAC(aad, ciphertext []byte) (high_mac, low_mac uint64) {
+	var r0, r1, r2, h0, h1, h2, c, c0, c1, c2 uint64
+	var i, j, l, phase uint
+	var d, d0, d1, d2 [4]uint64
+	var a0, a1, b0, b1, dst0, dst1, dst2, dst3 uint64
+
+	// Variables initialization: read 'r' and 's' as Little Endian unsigned int
+	// r &= 0xffffffc0ffffffc0ffffffc0fffffff as required by the Poly1305 specifications
+	//
+	// NOTE: we need 'r', 'h' and 'c' to be uint130 because of the required modulus 2^130 - 5
+	//       uint130(r) = 42 most significant bits(r2) + 44 middle bits(r1) + 44 less significant bits(r0)
+
+	// r0 = LSB 44 bits of 'r' as uint130
+	r0 = this.r0
+
+	// r1 = middle 44 bits of 'r' as uint130
+	r1 = this.r1
+
+	// r2 = MSB 42 bits of 'r' as uint130
+	r2 = this.r2
+
+	// h = 0 --> zero is already the default value for h0, h1 and h2, so nothing to do :-)
+
+	i = 0
+	l = uint(len(aad))
+	phase = aeadAAD
+	for phase != aeadEnd {
+		// Read 'c' from a chunk of 16 bytes (or less if not enough data) as a Little Endian unsigned integer (uint130)
+		// uint130(c) = 42 most significant bits(c2) + 44 middle bits(c1) + 44 less significant bits(c0)
+		c0 = 0
+		c1 = 0
+		c2 = 0
+		switch phase {
+		case aeadAAD:
+			for j = 0; (j < 16) && (i < l); j++ {
+				if j < 5 {
+					c0 |= uint64(aad[i]) << (j << 3)
+				} else if j == 5 {
+					c0 |= uint64(aad[i]) << 40
+					c1 |= uint64(aad[i]) >> 4
+				} else if j < 11 {
+					c1 |= uint64(aad[i]) << (4 + ((j - 6) << 3))
+				} else { // j >= 11
+					c2 |= uint64(aad[i]) << ((j - 11) << 3)
+				}
+				i++
+			}
+			if i >= l {
+				i = 0
+				l = uint(len(ciphertext))
+				phase = aeadCipherText
+			}
+			break
+		case aeadCipherText:
+			for j = 0; (j < 16) && (i < l); j++ {
+				if j < 5 {
+					c0 |= uint64(ciphertext[i]) << (j << 3)
+				} else if j == 5 {
+					c0 |= uint64(ciphertext[i]) << 40
+					c1 |= uint64(ciphertext[i]) >> 4
+				} else if j < 11 {
+					c1 |= uint64(ciphertext[i]) << (4 + ((j - 6) << 3))
+				} else { // j >= 11
+					c2 |= uint64(ciphertext[i]) << ((j - 11) << 3)
+				}
+				i++
+			}
+			if i >= l {
+				phase = aeadLengths
+			}
+			break
+		case aeadLengths:
+			a0 = uint64(len(aad))        // first 64 bit in little endian
+			a1 = uint64(len(ciphertext)) // last 64 bit in little endian
+			c0 = a0
+			c1 = ((a0 >> 44) & 0xfffff) | ((a1 << 20) & 0xffffff00000)
+			c2 = (a1 >> 24) & 0xffffffffff
+			phase = aeadEnd
+			break
+		}
+		c0 &= 0xfffffffffff
+		c1 &= 0xfffffffffff
+		c2 &= 0x3ffffffffff
+
+		// In AEAD mode, every chunk'size == 16 bytes so always add a 17th byte = 0x01
+		c2 |= 1 << 40
 
 		// for each chunk 'c', update 'h' like this:      h = ((h + c) * r) % ((2^130)-5)
 
--- crypto/poly1305_test.go
+++ crypto/poly1305_test.go
@@ -24,7 +24,7 @@ func Test_Poly1305(t *testing.T) {
 	}
 
 	// MAC = a8:06:1d:c1:30:51:36:c6:c2:2b:8b:af:0c:01:27:a9
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0xa8, 0x06, 0x1d, 0xc1, 0x30, 0x51, 0x36, 0xc6, 0xc2, 0x2b, 0x8b, 0xaf, 0x0c, 0x01, 0x27, 0xa9}) {
@@ -52,7 +52,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = make([]byte, 64)
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) {
@@ -102,7 +102,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = []byte("Any submission to the IETF intended by the Contributor for publication as all or part of an IETF Internet-Draft or RFC and any statement made within the context of an IETF activity is considered an \"IETF Contribution\". Such statements include oral statements in IETF sessions, as well as written and electronic communications made at any time or place, which are addressed to")
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0x36, 0xe5, 0xf6, 0xb5, 0xc5, 0xe0, 0x60, 0x70, 0xf0, 0xef, 0xca, 0x96, 0x22, 0x7a, 0x86, 0x3e}) {
@@ -152,7 +152,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = []byte("Any submission to the IETF intended by the Contributor for publication as all or part of an IETF Internet-Draft or RFC and any statement made within the context of an IETF activity is considered an \"IETF Contribution\". Such statements include oral statements in IETF sessions, as well as written and electronic communications made at any time or place, which are addressed to")
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0xf3, 0x47, 0x7e, 0x7c, 0xd9, 0x54, 0x17, 0xaf, 0x89, 0xa6, 0xb8, 0x79, 0x4c, 0x31, 0x0c, 0xf0}) {
@@ -186,7 +186,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = []byte("'Twas brillig, and the slithy toves\nDid gyre and gimble in the wabe:\nAll mimsy were the borogoves,\nAnd the mome raths outgrabe.")
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0x45, 0x41, 0x66, 0x9a, 0x7e, 0xaa, 0xee, 0x61, 0xe7, 0x08, 0xdc, 0x7c, 0xbc, 0xc5, 0xeb, 0x62}) {
@@ -210,7 +210,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) {
@@ -235,7 +235,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = []byte{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) {
@@ -265,7 +265,7 @@ func Test_Poly1305(t *testing.T) {
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 		0xF0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 		0x11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) {
@@ -295,7 +295,7 @@ func Test_Poly1305(t *testing.T) {
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 		0xFB, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE,
 		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) {
@@ -320,7 +320,7 @@ func Test_Poly1305(t *testing.T) {
 		t.Error(err)
 	}
 	m = []byte{0xFD, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0xFA, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}) {
@@ -352,7 +352,7 @@ func Test_Poly1305(t *testing.T) {
 		0x33, 0x94, 0xD7, 0x50, 0x5E, 0x43, 0x79, 0xCD, 1, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0x14, 0, 0, 0, 0, 0, 0, 0, 0x55, 0, 0, 0, 0, 0, 0, 0}) {
@@ -382,11 +382,92 @@ func Test_Poly1305(t *testing.T) {
 		0xE3, 0x35, 0x94, 0xD7, 0x50, 0x5E, 0x43, 0xB9, 0, 0, 0, 0, 0, 0, 0, 0,
 		0x33, 0x94, 0xD7, 0x50, 0x5E, 0x43, 0x79, 0xCD, 1, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	mac_h, mac_l = p.ComputeHash(m)
+	mac_h, mac_l = p.ComputeMAC(m)
 	binary.LittleEndian.PutUint64(mac, mac_l)
 	binary.LittleEndian.PutUint64(mac[8:], mac_h)
 	if !bytes.Equal(mac, []byte{0x13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) {
 		t.Errorf("Poly1305 : invalid mac")
 	}
 
+}
+
+func Test_ComputeAeadMAC(t *testing.T) {
+	var mac_h, mac_l uint64
+	var err error
+	var p *Poly1305
+	var cipher *ChaCha20Cipher
+	var buf [64]byte
+
+	// Test Vectors taken from RFC7539 Annexe 5 : http://tools.ietf.org/html/rfc7539
+
+	/*
+	   The key:
+	   000  1c 92 40 a5 eb 55 d3 8a f3 33 88 86 04 f6 b5 f0  ..@..U...3......
+	   016  47 39 17 c1 40 2b 80 09 9d ca 5c bc 20 70 75 c0  G9..@+....\. pu.
+
+	   Ciphertext:
+	   000  64 a0 86 15 75 86 1a f4 60 f0 62 c7 9b e6 43 bd  d...u...`.b...C.
+	   016  5e 80 5c fd 34 5c f3 89 f1 08 67 0a c7 6c 8c b2  ^.\.4\....g..l..
+	   032  4c 6c fc 18 75 5d 43 ee a0 9e e9 4e 38 2d 26 b0  Ll..u]C....N8-&.
+	   048  bd b7 b7 3c 32 1b 01 00 d4 f0 3b 7f 35 58 94 cf  ...<2.....;.5X..
+	   064  33 2f 83 0e 71 0b 97 ce 98 c8 a8 4a bd 0b 94 81  3/..q......J....
+	   080  14 ad 17 6e 00 8d 33 bd 60 f9 82 b1 ff 37 c8 55  ...n..3.`....7.U
+	   096  97 97 a0 6e f4 f0 ef 61 c1 86 32 4e 2b 35 06 38  ...n...a..2N+5.8
+	   112  36 06 90 7b 6a 7c 02 b0 f9 f6 15 7b 53 c8 67 e4  6..{j|.....{S.g.
+	   128  b9 16 6c 76 7b 80 4d 46 a5 9b 52 16 cd e7 a4 e9  ..lv{.MF..R.....
+	   144  90 40 c5 a4 04 33 22 5e e2 82 a1 b0 a0 6c 52 3e  .@...3"^.....lR>
+	   160  af 45 34 d7 f8 3f a1 15 5b 00 47 71 8c bc 54 6a  .E4..?..[.Gq..Tj
+	   176  0d 07 2b 04 b3 56 4e ea 1b 42 22 73 f5 48 27 1a  ..+..VN..B"s.H'.
+	   192  0b b2 31 60 53 fa 76 99 19 55 eb d6 31 59 43 4e  ..1`S.v..U..1YCN
+	   208  ce bb 4e 46 6d ae 5a 10 73 a6 72 76 27 09 7a 10  ..NFm.Z.s.rv'.z.
+	   224  49 e6 17 d9 1d 36 10 94 fa 68 f0 ff 77 98 71 30  I....6...h..w.q0
+	   240  30 5b ea ba 2e da 04 df 99 7b 71 4d 6c 6f 2c 29  0[.......{qMlo,)
+	   256  a6 ad 5c b4 02 2b 02 70 9b                       ..\..+.p.
+
+	   The nonce:
+	   000  00 00 00 00 01 02 03 04 05 06 07 08              ............
+
+	   The AAD:
+	   000  f3 33 88 86 00 00 00 00 00 00 4e 91              .3........N.
+
+	   Received Tag:
+	   000  ee ad 9d 67 89 0c bb 22 39 23 36 fe a1 85 1f 38  ...g..."9#6....8
+	*/
+	key := []byte{
+		0x1c, 0x92, 0x40, 0xa5, 0xeb, 0x55, 0xd3, 0x8a, 0xf3, 0x33, 0x88, 0x86, 0x04, 0xf6, 0xb5, 0xf0,
+		0x47, 0x39, 0x17, 0xc1, 0x40, 0x2b, 0x80, 0x09, 0x9d, 0xca, 0x5c, 0xbc, 0x20, 0x70, 0x75, 0xc0}
+	aad := []byte{0xf3, 0x33, 0x88, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4e, 0x91}
+	cipherText := []byte{
+		0x64, 0xa0, 0x86, 0x15, 0x75, 0x86, 0x1a, 0xf4, 0x60, 0xf0, 0x62, 0xc7, 0x9b, 0xe6, 0x43, 0xbd,
+		0x5e, 0x80, 0x5c, 0xfd, 0x34, 0x5c, 0xf3, 0x89, 0xf1, 0x08, 0x67, 0x0a, 0xc7, 0x6c, 0x8c, 0xb2,
+		0x4c, 0x6c, 0xfc, 0x18, 0x75, 0x5d, 0x43, 0xee, 0xa0, 0x9e, 0xe9, 0x4e, 0x38, 0x2d, 0x26, 0xb0,
+		0xbd, 0xb7, 0xb7, 0x3c, 0x32, 0x1b, 0x01, 0x00, 0xd4, 0xf0, 0x3b, 0x7f, 0x35, 0x58, 0x94, 0xcf,
+		0x33, 0x2f, 0x83, 0x0e, 0x71, 0x0b, 0x97, 0xce, 0x98, 0xc8, 0xa8, 0x4a, 0xbd, 0x0b, 0x94, 0x81,
+		0x14, 0xad, 0x17, 0x6e, 0x00, 0x8d, 0x33, 0xbd, 0x60, 0xf9, 0x82, 0xb1, 0xff, 0x37, 0xc8, 0x55,
+		0x97, 0x97, 0xa0, 0x6e, 0xf4, 0xf0, 0xef, 0x61, 0xc1, 0x86, 0x32, 0x4e, 0x2b, 0x35, 0x06, 0x38,
+		0x36, 0x06, 0x90, 0x7b, 0x6a, 0x7c, 0x02, 0xb0, 0xf9, 0xf6, 0x15, 0x7b, 0x53, 0xc8, 0x67, 0xe4,
+		0xb9, 0x16, 0x6c, 0x76, 0x7b, 0x80, 0x4d, 0x46, 0xa5, 0x9b, 0x52, 0x16, 0xcd, 0xe7, 0xa4, 0xe9,
+		0x90, 0x40, 0xc5, 0xa4, 0x04, 0x33, 0x22, 0x5e, 0xe2, 0x82, 0xa1, 0xb0, 0xa0, 0x6c, 0x52, 0x3e,
+		0xaf, 0x45, 0x34, 0xd7, 0xf8, 0x3f, 0xa1, 0x15, 0x5b, 0x00, 0x47, 0x71, 0x8c, 0xbc, 0x54, 0x6a,
+		0x0d, 0x07, 0x2b, 0x04, 0xb3, 0x56, 0x4e, 0xea, 0x1b, 0x42, 0x22, 0x73, 0xf5, 0x48, 0x27, 0x1a,
+		0x0b, 0xb2, 0x31, 0x60, 0x53, 0xfa, 0x76, 0x99, 0x19, 0x55, 0xeb, 0xd6, 0x31, 0x59, 0x43, 0x4e,
+		0xce, 0xbb, 0x4e, 0x46, 0x6d, 0xae, 0x5a, 0x10, 0x73, 0xa6, 0x72, 0x76, 0x27, 0x09, 0x7a, 0x10,
+		0x49, 0xe6, 0x17, 0xd9, 0x1d, 0x36, 0x10, 0x94, 0xfa, 0x68, 0xf0, 0xff, 0x77, 0x98, 0x71, 0x30,
+		0x30, 0x5b, 0xea, 0xba, 0x2e, 0xda, 0x04, 0xdf, 0x99, 0x7b, 0x71, 0x4d, 0x6c, 0x6f, 0x2c, 0x29,
+		0xa6, 0xad, 0x5c, 0xb4, 0x02, 0x2b, 0x02, 0x70, 0x9b}
+	noncePrefix := []byte{0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8}
+	if cipher, err = NewChaCha20Cipher(key, noncePrefix, 0); err != nil {
+		t.Error("Key Generator test for Poly1305 : error when calling NewChaCha20Cipher")
+	}
+	cipher.GetNextKeystream(&buf)
+	if p, err = NewPoly1305(buf[:]); err != nil {
+		t.Error(err)
+	}
+	mac_h, mac_l = p.ComputeAeadMAC(aad, cipherText)
+	mac := make([]byte, 16)
+	binary.LittleEndian.PutUint64(mac, mac_l)
+	binary.LittleEndian.PutUint64(mac[8:], mac_h)
+	if !bytes.Equal(mac, []byte{0xee, 0xad, 0x9d, 0x67, 0x89, 0x0c, 0xbb, 0x22, 0x39, 0x23, 0x36, 0xfe, 0xa1, 0x85, 0x1f, 0x38}) {
+		t.Error("ComputeAeadMAC : invalid MAC")
+	}
 }
