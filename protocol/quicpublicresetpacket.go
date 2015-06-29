2ebefbf2f790d3a8d8d54feff8fc3564c7128594
Romain Jacotin <romain.jacotin@orange.fr>
2015-06-29 19:31:51 +0200

    Finalize QUIC Public Reset packet parsing and serializing functions with successful go test

--- protocol/quicpublicresetpacket.go
+++ protocol/quicpublicresetpacket.go
@@ -3,51 +3,172 @@ package protocol
 import "encoding/binary"
 import "errors"
 
+/*
+
+     0        1        2        3        4         8
++--------+--------+--------+--------+--------+--   --+
+| Public |    Connection ID (64)                ...  | ->
+|Flags(8)|                                           |
++--------+--------+--------+--------+--------+--   --+
+
+     9       10       11        12       13      14
++--------+--------+--------+--------+--------+--------+---
+|      Quic Tag (32)                |  Tag value map      ... ->
+|         (PRST)                    |  (variable length)
++--------+--------+--------+--------+--------+--------+---
+
+
+Public flags:
++---+---+---+---+---+---+---+---+
+| 0 | 0 | SeqNum| ConnID|Rst|Ver|
++---+---+---+---+---+---+---+---+
+
+*/
+
+type QuicPublicResetNonceProof uint64
+
 type QuicPublicResetPacket struct {
 	msg            Message
-	nonce          []byte
+	nonceProof     QuicPublicResetNonceProof
 	rejectedSeqNum QuicPacketSequenceNumber
-	buffer         [8]byte
+	buffer         [16]byte
 }
 
 // Erase
 func (this *QuicPublicResetPacket) Erase() {
 	this.msg.tags = nil
 	this.msg.values = nil
-	this.nonce = nil
+	this.nonceProof = 0
 	this.rejectedSeqNum = 0
 }
 
 // ParseData
 func (this *QuicPublicResetPacket) ParseData(data []byte) (size int, err error) {
-	// NOT YET IMPLEMENTED !
-	err = errors.New("NOT YET IMPLEMENTED !")
+	var b bool
+	var buffer []byte
+
+	// Check minimum Public Reset packet size
+	l := len(data)
+	if l < (40) {
+		err = errors.New("QuicPublicResetPacket.ParseData : data size too small to contain Public Reset packet")
+		return
+	}
+	// Read uint32 message tag
+	this.msg.msgTag = MessageTag(binary.LittleEndian.Uint32(data))
+	size = 4
+	if this.msg.msgTag != TagPRST {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid Public Reset packet, PRST message tag required")
+		return
+	}
+	// Read uint16 number of entries and ignore next uint16 of padding
+	numEntries := uint16(binary.LittleEndian.Uint16(data[size:]))
+	if numEntries < 2 {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid Public Reset packet, RNON and RSEG tags required")
+		return
+	}
+	if numEntries > MaxMessageTagNumEntries {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid number of Tag entries in Public Reset packet (>128)")
+		return
+	}
+	size = 8
+	// Ask for next data size
+	needMoreData := int(8*numEntries) + size
+	if l < needMoreData {
+		err = errors.New("QuicPublicResetPacket.ParseData : data size too small to contain Public Reset packet")
+		return
+	}
+	// Allocate ressources for tag-offset pairs
+	this.msg.tags = make([]MessageTag, numEntries)
+	endOffsets := make([]uint32, numEntries)
+	for i := 0; i < int(numEntries); i++ {
+		// Read uint32 tag
+		this.msg.tags[i] = MessageTag(binary.LittleEndian.Uint32(data[size:]))
+		size += 4
+		// Read uint32 offset
+		endOffsets[i] = uint32(binary.LittleEndian.Uint32(data[size:]))
+		size += 4
+	}
+	// Ask for next data size
+	needMoreData += int(endOffsets[numEntries-1])
+	if l < needMoreData {
+		err = errors.New("QuicPublicResetPacket.ParseData : data size too small to contain Public Reset packet")
+		return
+	}
+	// Allocate ressources for tag-value pairs
+	this.msg.values = make([][]byte, numEntries)
+	// Read values
+	offset := 0
+	for i := 0; i < int(numEntries); i++ {
+		this.msg.values[i] = data[size+offset : uint32(size)+endOffsets[i]]
+		offset = int(endOffsets[i])
+	}
+	size += offset
+	// Do we have mandatory RNON tag/value pair ?
+	if b, buffer = this.msg.ContainsTag(TagRNON); !b {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid Public Reset packet, RNON tag required")
+		return
+	}
+	// Parse Nonce Proof
+	if len(buffer) != 8 {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid Public Reset packet, invalid RNON value size (no 64bit value size)")
+		return
+	}
+	this.nonceProof = QuicPublicResetNonceProof(binary.LittleEndian.Uint64(buffer))
+	// Do we have mandatory RSEQ tag/value pair ?
+	if b, buffer = this.msg.ContainsTag(TagRSEQ); !b {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid Public Reset packet, RSEQ tag required")
+		return
+	}
+	// Parse Rejected Sequence Number
+	if len(buffer) != 8 {
+		err = errors.New("QuicPublicResetPacket.ParseData : invalid Public Reset packet, invalid RSEQ value size (no 64bit value size)")
+		return
+	}
+	this.rejectedSeqNum = QuicPacketSequenceNumber(binary.LittleEndian.Uint64(buffer))
 	return
 }
 
 // GetSerializedSize
 func (this *QuicPublicResetPacket) GetSerializedSize() (size int) {
-	// NOT YET IMPLEMENTED !
+	size = int(this.msg.GetSerializeSize())
 	return
 }
 
 // GetSerializedData
 func (this *QuicPublicResetPacket) GetSerializedData(data []byte) (size int, err error) {
+
 	this.msg.msgTag = TagPRST
-	this.msg.AddTagValue(TagRNON, this.nonce)
-	binary.LittleEndian.PutUint64(this.buffer[:], uint64(this.rejectedSeqNum))
-	this.msg.AddTagValue(TagRSEQ, this.buffer[:])
+	// Add 'RNON' tag/value pair
+	binary.LittleEndian.PutUint64(this.buffer[0:8], uint64(this.nonceProof))
+	if b, _ := this.msg.ContainsTag(TagRNON); b {
+		this.msg.UpdateTagValue(TagRNON, this.buffer[0:8])
+	} else {
+		this.msg.AddTagValue(TagRNON, this.buffer[0:8])
+	}
+	// Add 'RSEQ' tag/value pair
+	binary.LittleEndian.PutUint64(this.buffer[8:16], uint64(this.rejectedSeqNum))
+	if b, _ := this.msg.ContainsTag(TagRSEQ); b {
+		this.msg.UpdateTagValue(TagRSEQ, this.buffer[8:16])
+	} else {
+		this.msg.AddTagValue(TagRSEQ, this.buffer[8:16])
+	}
+	size = int(this.msg.GetSerializeSize())
+	if len(data) < size {
+		err = errors.New("QuicPublicResetPacket.GetSerializedData : data size too small to contain Public Reset packet")
+		size = 0
+	}
+	copy(data, this.msg.GetSerialize())
 	return
 }
 
-// GetNonce
-func (this *QuicPublicResetPacket) GetNonce() []byte {
-	return this.nonce
+// GetNonceProof
+func (this *QuicPublicResetPacket) GetNonceProof() QuicPublicResetNonceProof {
+	return this.nonceProof
 }
 
-// SetNonce
-func (this *QuicPublicResetPacket) SetNonce(nonce []byte) {
-	this.nonce = nonce
+// SetNonceProof
+func (this *QuicPublicResetPacket) SetNonceProof(nonce QuicPublicResetNonceProof) {
+	this.nonceProof = nonce
 }
 
 // GetRejectedSequenceNumber
--- protocol/quicpublicresetpacket_test.go
+++ protocol/quicpublicresetpacket_test.go
@@ -1,11 +1,177 @@
 package protocol
 
 import "testing"
+import "bytes"
+
+/*
+
+     0        1        2        3        4         8
++--------+--------+--------+--------+--------+--   --+
+| Public |    Connection ID (64)                ...  | ->
+|Flags(8)|                                           |
++--------+--------+--------+--------+--------+--   --+
+
+     9       10       11        12       13      14
++--------+--------+--------+--------+--------+--------+---
+|      Quic Tag (32)                |  Tag value map      ... ->
+|         (PRST)                    |  (variable length)
++--------+--------+--------+--------+--------+--------+---
+
+
+Public flags:
++---+---+---+---+---+---+---+---+
+| 0 | 0 | SeqNum| ConnID|Rst|Ver|
++---+---+---+---+---+---+---+---+
+
+TagPRST = 0x54535250
+TagRNON = 0x4e4f4e52
+TagRSEQ = 0x51455352
+
+*/
+
+type testquicpublicresetpacket struct {
+	positiveTest   bool
+	data           []byte
+	nonceProof     QuicPublicResetNonceProof
+	rejectedseqnum QuicPacketSequenceNumber
+}
+
+var tests_quicpublicresetpacket = []testquicpublicresetpacket{
+	{true, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x08, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x10, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0x0a}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0a0b0c0daabbccdd},
+	// Bad message tag 'QRST'
+	{false, []byte{
+		0x51, 0x52, 0x53, 0x54, // Tag 'QRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x08, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x10, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0x0a}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0a0b0c0daabbccdd},
+	// Missing 'RNON' tag
+	{false, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4f, // Tag 'RNOO'
+		0x08, 0x00, 0x00, 0x00, //     'RNOO' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x10, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, // RNOO value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0x0a}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0a0b0c0daabbccdd},
+	// Bad 'RNON' value size (< 64bit)
+	{false, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x07, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x0f, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0x0a}, // Rejected Sequence Number
+		0xfebabecefedade, 0x0a0b0c0daabbccdd},
+	// Bad 'RNON' value size (> 64bit)
+	{false, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x09, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x11, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, 0x00, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0x0a}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0a0b0c0daabbccdd},
+	// Missing 'RSEQ' tag
+	{false, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x08, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x46, 0x51, // Tag 'RSFQ'
+		0x10, 0x00, 0x00, 0x00, //     'RSFQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0x0a}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0a0b0c0daabbccdd},
+	// Bad 'RSEQ' value size (< 64bit)
+	{false, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x08, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x0f, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0b0c0daabbccdd},
+	// Bad 'RSEQ' value size (> 64bit)
+	{false, []byte{
+		0x50, 0x52, 0x53, 0x54, // Tag 'PRST'
+		0x02, 0x00, 0x00, 0x00, // Num entries (uint16) + 2 bytes of padding
+		0x52, 0x4e, 0x4f, 0x4e, // Tag 'RNON'
+		0x08, 0x00, 0x00, 0x00, //     'RNON' offset
+		0x52, 0x53, 0x45, 0x51, // Tag 'RSEQ'
+		0x11, 0x00, 0x00, 0x00, //     'RSEQ' offset
+		0xde, 0xda, 0xfe, 0xce, 0xbe, 0xba, 0xfe, 0xca, // RNON value
+		0xdd, 0xcc, 0xbb, 0xaa, 0x0d, 0x0c, 0x0b, 0xca, 0x00}, // Rejected Sequence Number
+		0xcafebabecefedade, 0x0b0c0daabbccdd},
+}
 
 func Test_QuicPublicRestPacket_ParseData(t *testing.T) {
+	var reset QuicPublicResetPacket
 
+	for i, v := range tests_quicpublicresetpacket {
+		s, err := reset.ParseData(v.data)
+		if v.positiveTest {
+			if err != nil {
+				t.Errorf("QuicPublicRestPacket.ParseData : error %s in test %x with data[%v]%x", err, i, len(v.data), v.data)
+			}
+			if s != len(v.data) {
+				t.Errorf("QuicPublicRestPacket.ParseData : invalid size %v in test %x with data[%v]%x", s, i, len(v.data), v.data)
+			}
+			if v.nonceProof != reset.GetNonceProof() {
+				t.Errorf("QuicPublicRestPacket.ParseData : invalid proof %x in test %x with data[%v]%x", reset.GetNonceProof(), i, len(v.data), v.data)
+			}
+			if v.rejectedseqnum != reset.GetRejectedSequenceNumber() {
+				t.Errorf("QuicPublicRestPacket.ParseData : invalid rejected sequence number %x in test %x with data[%v]%x", reset.GetRejectedSequenceNumber(), i, len(v.data), v.data)
+			}
+		} else {
+			if err == nil {
+				t.Error("QuicPublicRestPacket.ParseData : missing error in test %x with data[%v]%x", i, len(v.data), v.data)
+			}
+		}
+	}
 }
 
 func Test_QuicPublicRestPacket_GetSerializedData(t *testing.T) {
+	var reset QuicPublicResetPacket
+
+	data := make([]byte, 40)
+	for i, v := range tests_quicpublicresetpacket {
+		if v.positiveTest {
+			reset.SetNonceProof(v.nonceProof)
+			reset.SetRejectedSequenceNumber(v.rejectedseqnum)
+			s, err := reset.GetSerializedData(data)
+			if err != nil {
+				t.Errorf("QuicPublicRestPacket.GetSerializedData = error %s while serialized data in test n°%v", err, i)
+			}
+			if s != len(v.data) {
+				t.Errorf("QuicPublicRestPacket.GetSerializedData = invalid serialized size in test n°%v with data[%v]%x", i, s, data[:s])
+			}
+			if !bytes.Equal(data[:s], v.data) {
+				t.Errorf("QuicPublicRestPacket.GetSerializedData = invalid serialized data in test n°%v with data[%v]%x", i, s, data[:s])
+			}
+			reset.Erase()
+		}
+	}
 
 }
